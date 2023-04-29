b7ae968dc55826f5e3abc83fa2903e2143876bfe
Alexander Dang <alexdang1993374@gmail.com>
2023-04-29 12:50:28 +0700

    feat: computer loader

--- 3d-portfolio/src/components/Loader.tsx
+++ 3d-portfolio/src/components/Loader.tsx
@@ -1,5 +1,25 @@
+import { Html, useProgress } from "@react-three/drei";
+
 const Loader = () => {
-  return <div>Loader</div>;
+  const progress = useProgress();
+  const percentage: number = (progress.loaded / progress.total) * 100;
+
+  return (
+    <Html>
+      <span className="canvas-load"></span>
+
+      <p
+        style={{
+          fontSize: 14,
+          color: "#f1f1f1",
+          fontWeight: 800,
+          marginTop: 40,
+        }}
+      >
+        {percentage.toFixed(2)}%
+      </p>
+    </Html>
+  );
 };
 
 export default Loader;
