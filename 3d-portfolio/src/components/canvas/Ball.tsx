906d05bacaf2d0071b4a39d4a5b7fd24833f1a3c
Alexander Dang <alexdang1993374@gmail.com>
2023-04-29 15:59:30 +0700

    feat: tech

--- 3d-portfolio/src/components/canvas/Ball.tsx
+++ 3d-portfolio/src/components/canvas/Ball.tsx
@@ -1,5 +1,61 @@
-const Ball = () => {
-  return <div>Ball</div>;
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import {
+  Decal,
+  Float,
+  OrbitControls,
+  Preload,
+  useTexture,
+} from "@react-three/drei";
+import CanvasLoader from "../Loader";
+
+interface IBallCanvas {
+  icon: string;
+}
+
+const Ball = ({ icon }: IBallCanvas) => {
+  const [decal] = useTexture([icon]);
+
+  return (
+    // @ts-expect-error: Suppressing TypeScript error due to incorrect library type definitions
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={0.25} />
+
+      <directionalLight position={[0, 0, 0.05]} />
+
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1, 1]} />
+
+        <meshStandardMaterial
+          color="#fff8eb"
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+
+        <Decal
+          position={[0, 0, 1]}
+          rotation={[2 * Math.PI, 0, 6.25]}
+          flatShading
+          map={decal}
+        />
+      </mesh>
+    </Float>
+  );
 };
 
-export default Ball;
+const BallCanvas = ({ icon }: IBallCanvas) => {
+  return (
+    <Canvas frameloop="demand" gl={{ preserveDrawingBuffer: true }}>
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false} />
+
+        <Ball icon={icon} />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default BallCanvas;
