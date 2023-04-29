f340dc8975ba11fe64375161c24b15c4ce287ff0
Alexander Dang <alexdang1993374@gmail.com>
2023-04-29 18:18:10 +0700

    feat: stars

--- 3d-portfolio/src/components/canvas/Earth.tsx
+++ 3d-portfolio/src/components/canvas/Earth.tsx
@@ -1,4 +1,4 @@
-import { OrbitControls, useGLTF } from "@react-three/drei";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import CanvasLoader from "../Loader";
@@ -29,6 +29,8 @@ const EarthCanvas = () => {
 
         <Earth />
       </Suspense>
+
+      <Preload all />
     </Canvas>
   );
 };
--- 3d-portfolio/src/components/canvas/Stars.tsx
+++ 3d-portfolio/src/components/canvas/Stars.tsx
@@ -1,5 +1,51 @@
-const Stars = () => {
-  return <div>Stars</div>;
+import { useRef, Suspense, ComponentProps } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Points, PointMaterial, Preload } from "@react-three/drei";
+// @ts-expect-error: Suppressing TypeScript error due to incorrect library type definitions
+import * as random from "maath/random/dist/maath-random.esm";
+import * as THREE from "three";
+
+type PointsProps = ComponentProps<typeof Points>;
+
+const Stars = (props: PointsProps) => {
+  const ref = useRef<THREE.Points | null>(null);
+
+  const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 });
+
+  useFrame((_, delta) => {
+    if (ref.current) {
+      ref.current.rotation.x -= delta / 10;
+      ref.current.rotation.y -= delta / 15;
+    }
+  });
+
+  return (
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+        <PointMaterial
+          transparent
+          color="#f272c8"
+          size={0.002}
+          sizeAttenuation
+          depthWrite={false}
+        />
+      </Points>
+    </group>
+  );
 };
 
-export default Stars;
+const StarsCanvas = () => {
+  return (
+    <div className="w-full h-auto absolute inset-0 z-[-1]">
+      <Canvas camera={{ position: [0, 0, 1] }}>
+        <Suspense fallback={null}>
+          <Stars />
+        </Suspense>
+
+        <Preload all />
+      </Canvas>
+    </div>
+  );
+};
+
+export default StarsCanvas;
