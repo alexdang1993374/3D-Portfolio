c59c4691debb48aab25ec2fe6b1f90490750ab17
Alexander Dang <alexdang1993374@gmail.com>
2023-04-29 13:10:34 +0700

    fix: model

--- 3d-portfolio/src/components/canvas/Computers.tsx
+++ 3d-portfolio/src/components/canvas/Computers.tsx
@@ -3,7 +3,11 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
+interface IComputers {
+  isMobile: boolean;
+}
+
+const Computers = ({ isMobile }: IComputers) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
@@ -23,8 +27,8 @@ const Computers = () => {
 
       <primitive
         object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, -1.5]}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -32,6 +36,29 @@ const Computers = () => {
 };
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event: MediaQueryListEvent) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the Listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <Canvas
       frameloop="demand"
@@ -46,7 +73,7 @@ const ComputersCanvas = () => {
           minPolarAngle={Math.PI / 2}
         />
 
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
 
       <Preload all />
