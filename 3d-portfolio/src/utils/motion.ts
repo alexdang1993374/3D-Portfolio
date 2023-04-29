eb16af724e6fb459d0a4faec61a10cce5629bc21
Alexander Dang <alexdang1993374@gmail.com>
2023-04-29 11:00:24 +0700

    feat: initialize project

---
+++ 3d-portfolio/src/utils/motion.ts
@@ -0,0 +1,109 @@
+type Direction = "left" | "right" | "up" | "down";
+type TransitionType = "spring" | "tween";
+
+interface Animation {
+  hidden: object;
+  show: object;
+}
+
+export const textVariant = (delay: number): Animation => {
+  return {
+    hidden: {
+      y: -50,
+      opacity: 0,
+    },
+    show: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        type: "spring" as TransitionType,
+        duration: 1.25,
+        delay: delay,
+      },
+    },
+  };
+};
+
+export const fadeIn = (
+  direction: Direction,
+  type: TransitionType,
+  delay: number,
+  duration: number
+): Animation => {
+  return {
+    hidden: {
+      x: direction === "left" ? 100 : direction === "right" ? -100 : 0,
+      y: direction === "up" ? 100 : direction === "down" ? -100 : 0,
+      opacity: 0,
+    },
+    show: {
+      x: 0,
+      y: 0,
+      opacity: 1,
+      transition: {
+        type: type,
+        delay: delay,
+        duration: duration,
+        ease: "easeOut",
+      },
+    },
+  };
+};
+
+export const zoomIn = (delay: number, duration: number): Animation => {
+  return {
+    hidden: {
+      scale: 0,
+      opacity: 0,
+    },
+    show: {
+      scale: 1,
+      opacity: 1,
+      transition: {
+        type: "tween" as TransitionType,
+        delay: delay,
+        duration: duration,
+        ease: "easeOut",
+      },
+    },
+  };
+};
+
+export const slideIn = (
+  direction: Direction,
+  type: TransitionType,
+  delay: number,
+  duration: number
+): Animation => {
+  return {
+    hidden: {
+      x: direction === "left" ? "-100%" : direction === "right" ? "100%" : 0,
+      y: direction === "up" ? "100%" : direction === "down" ? "100%" : 0,
+    },
+    show: {
+      x: 0,
+      y: 0,
+      transition: {
+        type: type,
+        delay: delay,
+        duration: duration,
+        ease: "easeOut",
+      },
+    },
+  };
+};
+
+export const staggerContainer = (
+  staggerChildren: number,
+  delayChildren?: number
+): Animation => {
+  return {
+    hidden: {},
+    show: {
+      transition: {
+        staggerChildren: staggerChildren,
+        delayChildren: delayChildren || 0,
+      },
+    },
+  };
+};
