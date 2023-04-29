9268d93e8032aa7448c7a37b4dde50bccfc77004
Alexander Dang <alexdang1993374@gmail.com>
2023-04-29 12:17:50 +0700

    feat: hero

--- 3d-portfolio/src/components/Navbar.tsx
+++ 3d-portfolio/src/components/Navbar.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import { styles } from "../styles";
+import { close, logo, menu } from "../assets";
 import { navLinks } from "../constants";
-import { logo, menu, close } from "../assets";
+import { styles } from "../styles";
 
 const Navbar = () => {
   const [active, setActive] = useState<string>("");
