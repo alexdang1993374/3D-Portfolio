a22951ca9df8cf8bb9fb202492d071698b7e6a08
Alexander Dang <alexdang1993374@gmail.com>
2023-04-29 21:15:02 +0700

    fix: service

--- 3d-portfolio/src/constants/index.ts
+++ 3d-portfolio/src/constants/index.ts
@@ -46,7 +46,7 @@ const services = [
     icon: web,
   },
   {
-    title: "React Native Developer",
+    title: "Mobile Developer",
     icon: mobile,
   },
   {
@@ -54,7 +54,7 @@ const services = [
     icon: backend,
   },
   {
-    title: "Content Creator",
+    title: "Mentor",
     icon: creator,
   },
 ];
