157596f347468b5cd6ce24f8a660deb868bdf0d9
Alexander Dang <alexdang1993374@gmail.com>
2023-05-16 23:12:06 +0700

    fix: icon link

--- 3d-portfolio/src/components/ProjectLink.tsx
+++ 3d-portfolio/src/components/ProjectLink.tsx
@@ -1,13 +1,21 @@
+import { useState } from "react";
+
 interface IProjectLink {
   link: string;
   image: string;
 }
 
 const ProjectLink = ({ link, image }: IProjectLink) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const hoverStyles = isHovered ? `green-pink-gradient` : "black-gradient";
+
   return (
     <div
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
       onClick={() => window.open(link, "_blank")}
-      className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer hover:opacity-[0.85]"
+      className={`${hoverStyles} w-10 h-10 rounded-full flex justify-center items-center cursor-pointer transition-colors duration-200`}
     >
       <img src={image} alt="github" className="w-1/2 h-1/2 object-contain" />
     </div>
